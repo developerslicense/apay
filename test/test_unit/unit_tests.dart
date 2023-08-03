302778de2c4feeccb4a1fe4d87d4606b3b40d4c6
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-03 14:00:18 +0600

    удалил ненужную функцию парсинга и тесты

--- test/test_unit/unit_tests.dart
+++ test/test_unit/unit_tests.dart
@@ -2,10 +2,8 @@ import 'test_for_data/get_number_cleared_tests.dart';
 import 'test_for_data/get_number_cleared_with_max_size_tests.dart';
 import 'test_for_data/mask_formatter_tests.dart';
 import 'test_for_data/money_tests.dart';
-import 'test_for_parsing_argument_on_start/test_parsing.dart';
 
 void unitTests() {
-  testParsing();
   testsGetNumberClearedWithMaxSize();
   testsGetNumberCleared();
   testInitMoney();
