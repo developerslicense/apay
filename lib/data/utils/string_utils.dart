a4e66e6cd87caa5a0c4422e27fb9cd0f33d3b5d9
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-17 11:07:52 +0600

    рефакторинг

--- lib/data/utils/string_utils.dart
+++ lib/data/utils/string_utils.dart
@@ -1,13 +1,3 @@
 bool isBlank(String? text) {
   return text?.trim().isEmpty ?? true;
 }
-
-String? getValueFromArguments(
-    List<String> resultArgument,
-    String key
-) {
-  return resultArgument
-      .firstWhere((element) => element.contains(key))
-      .split('=')
-  [1];
-}
