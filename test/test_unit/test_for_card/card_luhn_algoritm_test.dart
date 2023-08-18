3971804167a77592576f56fba8d16edd35237a7c
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-18 11:44:47 +0600

    правки тестов

--- test/test_unit/test_for_card/card_luhn_algoritm_test.dart
+++ test/test_unit/test_for_card/card_luhn_algoritm_test.dart
@@ -5,17 +5,17 @@ void testLuhnAlgorithm() {
   test('тест getMoneyCleared', (){
     // success
     expect(_validateCardNumWithLuhnAlgorithm("6250941006528599"), true);
-    expect(_validateCardNumWithLuhnAlgorithm("60115564485789458"), true);
     expect(_validateCardNumWithLuhnAlgorithm("6011000991300009"), true);
     expect(_validateCardNumWithLuhnAlgorithm("5425233430109903"), true);
     expect(_validateCardNumWithLuhnAlgorithm("2222420000001113"), true);
     expect(_validateCardNumWithLuhnAlgorithm("4263982640269299"), true);
     expect(_validateCardNumWithLuhnAlgorithm("4917484589897107"), true);
     expect(_validateCardNumWithLuhnAlgorithm("4001919257537193"), true);
-    expect(_validateCardNumWithLuhnAlgorithm("411111111111111"), false);
+    expect(_validateCardNumWithLuhnAlgorithm("4111111111111111"), true);
     expect(_validateCardNumWithLuhnAlgorithm("5392150002388575"), true);
 
     // failure
+    expect(_validateCardNumWithLuhnAlgorithm("41111111111111"), false);
     expect(_validateCardNumWithLuhnAlgorithm("378282246310005"), false);
     expect(_validateCardNumWithLuhnAlgorithm("411111111111112"), false);
     expect(_validateCardNumWithLuhnAlgorithm("411111111111113"), false);
